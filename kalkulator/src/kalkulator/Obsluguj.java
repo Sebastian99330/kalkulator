7f0bbeb33b370f61fbb18d3de89a3787c8df94d9
Sebastian <Sebastian@DESKTOP-C45TLBC>
2017-03-20 02:48:44 +0100

    .

--- kalkulator/src/kalkulator/Obsluguj.java
+++ kalkulator/src/kalkulator/Obsluguj.java
@@ -1,7 +1,7 @@
 package kalkulator;
 
-public interface Obsluguj {	//interfejs po którym odziedzicz¹ klasy Klawiatura i Plik.
-	//sam nie wiem sk¹d ta g³upia nazwa, coœ w stylu - obs³uguj przyjmowanie linijki-dzia³ania.
+public interface Obsluguj {	//obs³uguj przyjmowanie linijki-dzia³ania.
+	//interfejs po którym odziedzicz¹ klasy Klawiatura i Plik.
 	//klasy które ten interfejs implementuj¹, maj¹ w okreœlony przez siebie sposób (z klawiatury / z pliku) 
 	//przyj¹æ stringa - jedn¹ linijkê opisuj¹c¹ dzia³anie np 22-4, i przekazaæ j¹ do klasy Analiza
 	//do metody Operuj ¿eby ta metoda rozebra³a podanego stringa na czêœci pierwsze (dwie liczby i operator),
